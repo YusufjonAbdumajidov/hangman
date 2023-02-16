6ff6a809ef8c65444df41afb5102e64492cee827
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-16 11:30:23 +0300

    new changes added

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -49,8 +49,40 @@ const LEFT_ARM = (
         background: "#111",
         position: "absolute",
         top: "150px",
-        right: "0px",
+        right: "10px",
         rotate: "30deg",
+        transformOrigin: "right bottom"
+    }}
+    />
+)
+
+
+const RIGHT_LEG = (
+    <div 
+    style={{
+        width: "100px",
+        height: "10px",
+        background: "#111",
+        position: "absolute",
+        top: "210px",
+        right: "-90px",
+        rotate: "60deg",
+        transformOrigin: "left bottom"
+    }}
+    />
+)
+
+
+const LEFT_LEG = (
+    <div 
+    style={{
+        width: "100px",
+        height: "10px",
+        background: "#111",
+        position: "absolute",
+        top: "210px",
+        right: "0px",
+        rotate: "-60deg",
         transformOrigin: "right bottom"
     }}
     />
@@ -63,6 +95,8 @@ const HangmanDrawing = () => {
        {BODY}
        {RIGHT_ARM}
        {LEFT_ARM}
+       {RIGHT_LEG}
+       {LEFT_LEG}
       <div style={{ position: "absolute",  height: "50px", width: "10px", background: "#111", top: "0", right: "0" }} />
       <div style={{ height: "10px", width: "200px", background: "#111", marginLeft: "120px" }} />
       <div style={{ height: "400px", width: "10px", background: "#111", marginLeft: "120px" }} />
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import "./keyboard.scss";
+
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+];
 
 const Keyboard = () => {
   return (
-    <div>
-      
+    <div style={{ 
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", 
+    gap: ".5rem" }}>
+      {KEYS.map(key => {
+            return (
+                <button className='btn' key={key}>{key}</button>
+            )
+      })}
     </div>
   )
 }
